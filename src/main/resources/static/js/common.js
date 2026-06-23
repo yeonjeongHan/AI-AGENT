18acff749626c8c959deae57ca233d01a16db8e3
82224075 <82224075@RP8222407501.corp.ktad.kt.com>
2026-06-23 13:51:58 +0900

    test

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = location.pathname.split("/").pop() || "index.html";
+
+    document.querySelectorAll(".menu li").forEach(li => {
+        const a = li.querySelector("a");
+        if (a && a.getAttribute("href") === currentPage) {
+            li.classList.add("active");
+        }
+    });
+});
